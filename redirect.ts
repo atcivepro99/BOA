3f7385ef960f1ca34ce8c5a379f2453e957c2ddc
atcivepro99 <atcivepro@gmail.com>
2025-11-18 07:25:25 -0800

    Update redirect URL in redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://bulkworldinfinity.com/page/index.php"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://file-bt5g.vercel.app"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
